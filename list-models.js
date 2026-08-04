11e8ab2eae1e928b3edb25c935cf9a0ea9ddbd9f
Mukesh Chaudhari <mukeshchaudhari1731@gmail.com>
2026-08-04 13:26:23 +0530

    Update list-models.js

--- list-models.js
+++ list-models.js
@@ -6,7 +6,7 @@ async function listModels() {
     try {
         // Since we don't know the exact method in this SDK, we might have to use raw fetch.
         // Let's try raw fetch to Gemini API to list models.
-        const response = await fetch(`https://generativelanguage.googleapis.com/v1beta/models?key=AQ.Ab8RN6J9hlJ9-djZnQUr_s1seyiJrcj6XYU_TId-KqyXDkS8Fw`);
+        const response = await fetch(`https://generativelanguage.googleapis.com/v1beta/models?key=${process.env.GEMINI_API_KEY}`);
         const data = await response.json();
         
         console.log("Supported Models for generateContent:");
